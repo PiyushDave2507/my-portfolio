c1f9b836ba7cf28e1373f6337fc3a029b1f7ddde
Piyush Dave <piyushdave2023@gmail.com>
2025-06-09 20:36:48 +0530

    make navbar

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { TbMenu2 } from 'react-icons/tb';
+import { IoClose } from 'react-icons/io5';
+import './style.css';
+
+const Index = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="nav_brand">
+        <Link to="/" className="nav_logo">
+          <span>Piyush</span>
+        </Link>
+      </div>
+
+      <ul className={`nav_list ${isOpen ? 'show-menu' : 'nav_list'}`}>
+        <li className="nav_item">
+          <NavLink to="/" className="nav_link" onClick={toggleMenu}>Home</NavLink>
+        </li>
+        <li className="nav_item">
+          <NavLink to="/about" className="nav_link" onClick={toggleMenu}>About</NavLink>
+        </li>
+        <li className="nav_item">
+          <NavLink to="/services" className="nav_link" onClick={closeMenu}>Services</NavLink>
+        </li>
+        <li className="nav_item">
+          <NavLink to="/project" className="nav_link" onClick={closeMenu}>Project</NavLink>
+        </li>
+        <li className="nav_item">
+          <NavLink to="/contact" className="nav_link" onClick={closeMenu}>Contact</NavLink>
+        </li>
+      </ul>
+
+      <div className="nav_toggle" onClick={closeMenu}>
+        {isOpen ? <IoClose size={24} /> : <TbMenu2 size={24} />}
+      </div>
+    </nav>
+  );
+};
+
+export default Index;
