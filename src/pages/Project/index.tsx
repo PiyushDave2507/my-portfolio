fdc8341a61de9587e7dee975a9cae5907d59b546
Piyush Dave <piyushdave2023@gmail.com>
2025-06-11 14:22:05 +0530

    all new changes done

---
+++ src/pages/Project/index.tsx
@@ -0,0 +1,11 @@
+import './style.css'
+
+function Project() {
+  return (
+    <div className='section'>
+      <h1 className='text-3xl'>Project Page</h1>
+    </div>
+  )
+}
+
+export default Project
