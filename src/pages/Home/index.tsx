31289d1238e8e3e93ff4f691a950f9decb79fcd1
Piyush Dave <piyushdave2023@gmail.com>
2025-06-12 11:08:38 +0530

    make a simple home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,27 @@
-import './style.css'
+import './style.css';
+import HomeLogo from '../../assets/images/homelogo.jpg';
 
 function Home() {
   return (
-    <div className='section'>
-      <h1 className='text-3xl'>Home Page</h1>
+    <div className="section">
+      <div className="left">
+        <h2 className="tagline">Full Stack Developer</h2>
+        <h1 className="title">Hello I'm Piyush Dave</h1>
+        <p className="description">
+          Hi, I'm Piyush Dave — a passionate Full Stack Web Developer with a knack for crafting seamless,
+          responsive, and scalable web applications. From designing intuitive front-end interfaces to building
+          powerful back-end systems.
+        </p>
+        <div className="buttons">
+          <button className="btn-primary">Let’s Talk</button>
+          <button className="btn-secondary">View Projects</button>
+        </div>
+      </div>
+      <div className="right">
+        <img src={HomeLogo} alt="Home Logo" className="img" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
