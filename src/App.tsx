43c1590ef80c2e4001bf615c0370558c6d3eca1c
Piyush Dave <piyushdave2023@gmail.com>
2025-06-23 12:04:39 +0530

    remove unwanted file

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Index from './components/navbar/index'
-import {Home, Services, Project, Contact} from './pages'
+import {Home, Project, Contact} from './pages'
 
 import './App.css'
 
@@ -12,11 +12,6 @@ function App() {
       <Index />
       <Routes>
         <Route path='/' element={<Home />} />
-<<<<<<< HEAD
-        
-=======
->>>>>>> c77b27862229af3368779406f8288793a66bd124
-        <Route path='/services' element={<Services />} />
         <Route path='/project' element={<Project />} />
         <Route path='/contact' element={<Contact />} />
       </Routes>
