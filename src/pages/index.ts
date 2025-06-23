a271e705a48a74a89258f2a1a8c0292aa1889ee5
Piyush Dave <piyushdave2023@gmail.com>
2025-06-23 11:58:37 +0530

    remove unwanted file

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,3 @@
 export {default as Home } from './Home/index'
 export {default as Contact } from './Contact/index'
 export {default as Project } from './Project/index'
-export {default as Services } from './Services/index'
